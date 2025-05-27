fface86bf6248c49170ac04dd1e3d469e6bdb46b
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2025-05-27 10:47:04 +0000

    sventh commit

--- memories/server/index.js
+++ memories/server/index.js
@@ -1,34 +1,74 @@
+// import express from 'express';
+// import bodyParset from 'body-parser';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import postRoutes from './routes/posts.js';
+// import userRoutes from './routes/user.js';
+
+// const app = express();
+// dotenv.config();
+
+// app.use(cors());
+// app.use(bodyParset.json({ limit:"30mb",extended: true}));
+// app.use(bodyParset.urlencoded({ limit:"30mb",extended: true}));
+// app.use(express.json());
+
+
+// const PORT = process.env.PORT || 5000;
+// app.get('/',(req,res)=>{
+//     res.send('Hello to Memories API.');
+// })
+// // mongoose.connect(CONNECTION_URL)
+// //   .then(() => console.log('MongoDB connected'))
+// //   .catch(err => console.error(err));
+
+// app.use('/posts',postRoutes);
+// app.use('/user',userRoutes);
+
+// mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser : true, useUnifiedTopology: true})
+//     .then(() => { app.listen(PORT, () => console.log(`server running on port : ${PORT}`))   })
+//     .catch((err)=>{ console.log(err);
+// })
+
+// // mongoose.set('useFindAndModify', false);
+
+
+
 import express from 'express';
-import bodyParset from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/user.js';
+import serverless from 'serverless-http';
 
-const app = express();
+import postRoutes from '../routes/posts.js';
+import userRoutes from '../routes/user.js';
+
 dotenv.config();
 
+const app = express();
+
+// Middleware
 app.use(cors());
-app.use(bodyParset.json({ limit:"30mb",extended: true}));
-app.use(bodyParset.urlencoded({ limit:"30mb",extended: true}));
-app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
+// Routes
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel Serverless Express!');
+});
 
-const PORT = process.env.PORT || 5000;
-app.get('/',(req,res)=>{
-    res.send('Hello to Memories API.');
-})
-// mongoose.connect(CONNECTION_URL)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
+// MongoDB connection
+let isConnected = false;
+async function connectDB() {
+  if (!isConnected) {
+    await mongoose.connect(process.env.CONNECTION_URL);
+    isConnected = true;
+  }
+}
+await connectDB();
 
-app.use('/posts',postRoutes);
-app.use('/user',userRoutes);
-
-mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser : true, useUnifiedTopology: true})
-    .then(() => { app.listen(PORT, () => console.log(`server running on port : ${PORT}`))   })
-    .catch((err)=>{ console.log(err);
-})
-
-// mongoose.set('useFindAndModify', false);
+export const handler = serverless(app);
