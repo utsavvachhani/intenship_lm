32ac502c93a66741761a4a72ecd9e4245d94986f
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2025-06-04 13:12:03 +0000

    14th commit

--- memories/server/api/index.js
+++ memories/server/api/index.js
@@ -1,26 +1,30 @@
-// index.js
-
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import serverless from "serverless-http";
-
-// Load environment variables
-dotenv.config();
+import express from 'express';
+import bodyParset from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
-
-// Middlewares
+dotenv.config();
 app.use(cors());
+
+
+app.use(bodyParset.json({ limit:"30mb",extended: true}));
+app.use(bodyParset.urlencoded({ limit:"30mb",extended: true}));
 app.use(express.json());
 
-// Sample Route
-app.get("/", (req, res) => {
-  res.json({ message: "Serverless API is working on Vercel!" });
-});
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
 
-// Add more routes below
-// app.post("/api/something", (req, res) => { ... });
+const PORT = process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send('Hello to Memories API.');
+})
+mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser : true, useUnifiedTopology: true})
+    .then(() => { app.listen(PORT, () => console.log(`server running on port : ${PORT}`))   })
+    .catch((err)=>{ console.log(err);
+})
 
-// Export the serverless handler
-export const handler = serverless(app);
+// mongoose.set('useFindAndModify', false);
