5759b2383315c33b17e228bea0ae31f06a8bec7b
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2025-06-05 04:52:18 +0000

    15th commit

--- memories/server/index.js
+++ memories/server/index.js
@@ -1,23 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParset from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 dotenv.config();
 app.use(cors());
 
 
-app.use(bodyParser.json({ limit:"30mb",extended: true}));
-app.use(bodyParser.urlencoded({ limit:"30mb",extended: true}));
+app.use(bodyParset.json({ limit:"30mb",extended: true}));
+app.use(bodyParset.urlencoded({ limit:"30mb",extended: true}));
 app.use(express.json());
 
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send('Hello to Memories API.');
